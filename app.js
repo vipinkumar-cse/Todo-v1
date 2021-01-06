ae4bb225432476b8558550ea7eee0c2a73a89f66
vipinkumar-cse <70096266+vipinkumar-cse@users.noreply.github.com>
2021-01-06 20:27:00 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,46 @@
+const express = require('express')
+const bodyParser = require("body-parser")
+
+const app = express();
+app.use(express.static('public'))
+var items = [];
+app.use(bodyParser.urlencoded({extended : true}))
+app.set("view engine", 'ejs');
+
+var daycode = new Date();
+var day;
+
+
+app.get("/", function(req, res) {
+
+var options = {
+  weekday : "long",
+  day : "numeric",
+  month : "long"
+
+}
+    day = daycode.toLocaleDateString("en-US", options);
+
+res.render('list', {dayofweek : day, newitems : items})
+  }
+)
+
+app.post("/", function(request, response){
+  console.log(request.body.todo);
+  var item = request.body.todo;
+  items.push(item)
+  response.redirect('/')
+
+})
+app.post('/clear', function(req, res){
+  items = '';
+  res.redirect('/')
+})
+
+
+app.listen(3000, function(req, res) {
+
+
+  console.log("server is Running ");
+
+})
